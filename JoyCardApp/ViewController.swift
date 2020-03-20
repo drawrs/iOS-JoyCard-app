44e4796a64147bebc2104d216adce39df48679be
Rizal Hilman <rizal@udacoding.com>
2020-03-20 15:11:04 +0700

    Initial Commit

---
+++ JoyCardApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JoyCardApp
+//
+//  Created by Rizal Hilman on 20/03/20.
+//  Copyright © 2020 Rizal Hilman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
