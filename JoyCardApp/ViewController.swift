3d3b601c8692a4c19b2a65a63fd7728bcac06cb0
Rizal Hilman <rizal@udacoding.com>
2020-03-23 19:11:55 +0700

    initial commit

--- JoyCardApp/ViewController.swift
+++ JoyCardApp/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // DROP YOUR OUTLET HERE!
+    @IBOutlet weak var viewCardContainer: UIView!
+    @IBOutlet weak var imageFunnyFace: UIImageView!
+    @IBOutlet weak var buttonRandomizeCard: UIButton!
+    
+    // MARK: Array of images & colors
+    var joyImages = [UIImage(named: "JoyFace1"), UIImage(named: "JoyFace2"), UIImage(named: "JoyFace3")]
+    var joyColors = [UIColor.systemYellow, UIColor.systemBlue, UIColor.systemGreen,  UIColor.systemRed]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // MARK: Modify object corner radious to make it rounded
+        viewCardContainer.layer.cornerRadius = 30
+        buttonRandomizeCard.layer.cornerRadius = 10
+        
     }
-
-
+    
+    // DROP YOUR ACTION HERE
+    @IBAction func buttonRandomizeCardTapped(_ sender: Any) {
+        //Your code here!
+        
+        // MARK: Randomize the array item position
+        joyImages.shuffle()
+        joyColors.shuffle()
+        
+        // MARK: Get the first item then apply to the views
+        // Note: The First item will alawys change when user tap the button
+        imageFunnyFace.image = joyImages[0]
+        view.backgroundColor = joyColors[0]
+    }
+    
+    
 }
 
